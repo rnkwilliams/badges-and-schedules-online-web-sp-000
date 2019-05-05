90b8ed9a0f97677af66af12743a8a0d1e5f61ece
Ranika Williams <rnkprice07@gmail.com>
2019-05-05 20:24:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,15 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  message_array = []
+  array.each do |name|
+    message_array.push(badge_maker(name))
+  end
+  return message_array
+end
+
+def assign_rooms(array)
+  room_assignments = []
+  array.each do
