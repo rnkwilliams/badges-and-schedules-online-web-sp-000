cccea1fbebb38d21a3b6df3559419ea1eb601a06
Ranika Williams <rnkprice07@gmail.com>
2019-05-05 20:14:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speakers)
